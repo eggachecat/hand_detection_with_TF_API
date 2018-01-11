cd8e1db53798e0c49667f3a8561d6ee4e7ce98a1
sunao <sunao_0626@hotmail.com>
2018-01-12 04:14:42 +0800

    Enable inference

--- inference.py
+++ inference.py
@@ -1,122 +1,158 @@
-
-# coding: utf-8
 import numpy as np
 import os
-import six.moves.urllib as urllib
-import sys
-import tarfile
 import tensorflow as tf
-import zipfile
-
-from collections import defaultdict
-from io import StringIO
-from matplotlib import pyplot as plt
 from PIL import Image
-import skimage.io as img_io
+import PIL.Image as Image
+import PIL.ImageColor as ImageColor
+import PIL.ImageDraw as ImageDraw
+import PIL.ImageFont as ImageFont
+import pylab as plt
+import skimage.io
 
-# # check tensorflow Version
-print(tf.__version__)
-if tf.__version__ < '1.4.0':
-  raise ImportError('Please upgrade your tensorflow installation to v1.4.* or later!')
-
-# This is needed to display the images.
-# get_ipython().run_line_magic('matplotlib', 'inline')
-
-# This is needed since the notebook is stored in the object_detection folder.
-sys.path.append("..")
-from utils import label_map_util
-from utils import visualization_utils as vis_util
-
-
-# What model to download.
-MODEL_NAME = 'hand_detection'
-
-# Path to frozen detection graph. This is the actual model that is used for the object detection.
-PATH_TO_CKPT = MODEL_NAME + '/frozen_inference_graph.pb'
-
-# List of the strings that is used to add correct label for each box.
-PATH_TO_LABELS = './training/label_map.pbtxt'
-print(PATH_TO_LABELS)
-
-NUM_CLASSES = 2
-
-# # Detection
-PATH_TO_TEST_IMAGES_DIR = 'test_images'
+PATH_TO_CKPT = './models/hand_detection/frozen_inference_graph.pb'
+PATH_TO_TEST_IMAGES_DIR = './data/'
 TEST_IMAGE_PATHS = [
-    os.path.join(PATH_TO_TEST_IMAGES_DIR, filename) for filename in  os.listdir(PATH_TO_TEST_IMAGES_DIR)]
+    os.path.join(PATH_TO_TEST_IMAGES_DIR, filename) for filename in os.listdir(PATH_TO_TEST_IMAGES_DIR)]
 
-# Size, in inches, of the output images.
-IMAGE_SIZE = (12, 8)
+INT_TO_CLASS = {
+    1: "L",
+    2: "R"
+}
 
-PATH_TO_SAVE_IMAGES_DIR = 'test_result'
+CLASS_TO_COLOR = {
+    "L": "green",
+    "R": "red"
+}
+
+
+def draw_bounding_box_on_image(image,
+                               ymin,
+                               xmin,
+                               ymax,
+                               xmax,
+                               color='red',
+                               thickness=4,
+                               display_str_list=(),
+                               use_normalized_coordinates=True):
+    """Adds a bounding box to an image.
+
+    Each string in display_str_list is displayed on a separate line above the
+    bounding box in black text on a rectangle filled with the input 'color'.
+    If the top of the bounding box extends to the edge of the image, the strings
+    are displayed below the bounding box.
+
+    Args:
+      image: a PIL.Image object.
+      ymin: ymin of bounding box.
+      xmin: xmin of bounding box.
+      ymax: ymax of bounding box.
+      xmax: xmax of bounding box.
+      color: color to draw bounding box. Default is red.
+      thickness: line thickness. Default value is 4.
+      display_str_list: list of strings to display in box
+                        (each to be shown on its own line).
+      use_normalized_coordinates: If True (default), treat coordinates
+        ymin, xmin, ymax, xmax as relative to the image.  Otherwise treat
+        coordinates as absolute.
+    """
+    draw = ImageDraw.Draw(image)
+    im_width, im_height = image.size
+    if use_normalized_coordinates:
+        (left, right, top, bottom) = (xmin * im_width, xmax * im_width,
+                                      ymin * im_height, ymax * im_height)
+    else:
+        (left, right, top, bottom) = (xmin, xmax, ymin, ymax)
+    draw.line([(left, top), (left, bottom), (right, bottom),
+               (right, top), (left, top)], width=thickness, fill=color)
+    try:
+        font = ImageFont.truetype('arial.ttf', 24)
+    except IOError:
+        font = ImageFont.load_default()
+
+    # If the total height of the display strings added to the top of the bounding
+    # box exceeds the top of the image, stack the strings below the bounding box
+    # instead of above.
+    display_str_heights = [font.getsize(ds)[1] for ds in display_str_list]
+    # Each display_str has a top and bottom margin of 0.05x.
+    total_display_str_height = (1 + 2 * 0.05) * sum(display_str_heights)
+
+    if top > total_display_str_height:
+        text_bottom = top
+    else:
+        text_bottom = bottom + total_display_str_height
+    # Reverse list and print from bottom to top.
+    for display_str in display_str_list[::-1]:
+        text_width, text_height = font.getsize(display_str)
+        margin = np.ceil(0.05 * text_height)
+        draw.rectangle(
+            [(left, text_bottom - text_height - 2 * margin), (left + text_width,
+                                                              text_bottom)],
+            fill=color)
+        draw.text(
+            (left + margin, text_bottom - text_height - margin),
+            display_str,
+            fill='black',
+            font=font)
+        text_bottom -= text_height - 2 * margin
 
 
 def load_image_into_numpy_array(image):
-  (im_width, im_height) = image.size
-  return np.array(image.getdata()).reshape(
-      (im_height, im_width, 3)).astype(np.uint8)
+    (im_width, im_height) = image.size
+    return np.array(image.getdata()).reshape(
+        (im_height, im_width, 3)).astype(np.uint8)
 
 
-def test():
+def infer_phase_1():
     # ## Load a (frozen) Tensorflow model into memory.
     detection_graph = tf.Graph()
     with detection_graph.as_default():
-      od_graph_def = tf.GraphDef()
-      with tf.gfile.GFile(PATH_TO_CKPT, 'rb') as fid:
-        serialized_graph = fid.read()
-        od_graph_def.ParseFromString(serialized_graph)
-        tf.import_graph_def(od_graph_def, name='')
-
-    # ## Loading label map
-    label_map = label_map_util.load_labelmap(PATH_TO_LABELS)
-    categories = label_map_util.convert_label_map_to_categories(
-        label_map, max_num_classes=NUM_CLASSES, use_display_name=True)
-    category_index = label_map_util.create_category_index(categories)
+        od_graph_def = tf.GraphDef()
+        with tf.gfile.GFile(PATH_TO_CKPT, 'rb') as fid:
+            serialized_graph = fid.read()
+            od_graph_def.ParseFromString(serialized_graph)
+            tf.import_graph_def(od_graph_def, name='')
 
     # # begin test image
+    image_list = []
+    box_list = []
+    score_list = []
+    classes_list = []
+    num_list = []
+
     with detection_graph.as_default():
-      with tf.Session(graph=detection_graph) as sess:
-        # Definite input and output Tensors for detection_graph
-        image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
-        # Each box represents a part of the image where a particular object was detected.
-        detection_boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
-        # Each score represent how level of confidence for each of the objects.
-        # Score is shown on the result image, together with the class label.
-        detection_scores = detection_graph.get_tensor_by_name('detection_scores:0')
-        detection_classes = detection_graph.get_tensor_by_name('detection_classes:0')
-        num_detections = detection_graph.get_tensor_by_name('num_detections:0')
-        for image_idx, image_path in enumerate(TEST_IMAGE_PATHS):
-          image = Image.open(image_path)
-          # the array based representation of the image will be used later in order to prepare the
-          # result image with boxes and labels on it.
-          image_np = load_image_into_numpy_array(image)
-          # Expand dimensions since the model expects images to have shape: [1, None, None, 3]
-          image_np_expanded = np.expand_dims(image_np, axis=0)
-          # Actual detection.
-          (boxes, scores, classes, num) = sess.run(
-              [detection_boxes, detection_scores, detection_classes, num_detections],
-              feed_dict={image_tensor: image_np_expanded})
-          #print(boxes, scores, classes, num)
-        # Visualization of the results of a detection.
-          result_image = vis_util.visualize_boxes_and_labels_on_image_array(
-              image_np,
-              np.squeeze(boxes),
-              np.squeeze(classes).astype(np.int32),
-              np.squeeze(scores),
-              category_index,
-              use_normalized_coordinates=True,
-              min_score_thresh=np.sort(scores, axis=None)[-3],
-              line_thickness=8)
+        with tf.Session(graph=detection_graph) as sess:
+            # Definite input and output Tensors for detection_graph
+            image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
+            # Each box represents a part of the image where a particular object was detected.
+            detection_boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
+            # Each score represent how level of confidence for each of the objects.
+            # Score is shown on the result image, together with the class label.
+            detection_scores = detection_graph.get_tensor_by_name('detection_scores:0')
+            detection_classes = detection_graph.get_tensor_by_name('detection_classes:0')
+            num_detections = detection_graph.get_tensor_by_name('num_detections:0')
+            for image_idx, image_path in enumerate(TEST_IMAGE_PATHS):
+                image = Image.open(image_path)
+                # the array based representation of the image will be used later in order to prepare the
+                # result image with boxes and labels on it.
+                image_np = load_image_into_numpy_array(image)
+                # Expand dimensions since the model expects images to have shape: [1, None, None, 3]
+                image_np_expanded = np.expand_dims(image_np, axis=0)
+                # Actual detection.
+                (boxes, scores, classes, num) = sess.run(
+                    [detection_boxes, detection_scores, detection_classes, num_detections],
+                    feed_dict={image_tensor: image_np_expanded})
 
-          img_io.imsave(
-             '{}/{}.png'.format(PATH_TO_SAVE_IMAGES_DIR, image_idx),
-             result_image 
-          )
-
-          # plt.figure(figsize=IMAGE_SIZE)
-          # plt.imshow(image_np)
-          # plt.show()
+                threshold = np.sort(scores, axis=None)[-3]
+                threshold = threshold if threshold > 0.01 else 0.01
+                for i in range(boxes.shape[1]):
+                    box = boxes[0][i]
+                    if scores[0][i] > threshold:
+                        ymin, xmin, ymax, xmax = box
+                        class_ = INT_TO_CLASS[classes[0][i]]
+                        draw_bounding_box_on_image(image, ymin, xmin, ymax, xmax, color=CLASS_TO_COLOR[class_],
+                                                   display_str_list=[class_ + ": {}".format(scores[0][i])])
+                image.show()
 
 
 if __name__ == "__main__":
-    test()
+    infer_phase_1()
