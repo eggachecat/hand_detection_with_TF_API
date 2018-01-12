d63dc7dfea060b11cb6d988c4d4b8d1d0459cace
rainyren <renokx@gmail.com>
2018-01-13 02:17:11 +0800

    add bright augment

--- data_augment/flip.py
+++ data_augment/flip.py
@@ -3,22 +3,42 @@ import argparse
 import os
 import glob
 import json
-from PIL import Image
+from PIL import Image, ImageEnhance
 
 parser = argparse.ArgumentParser()
 parser.add_argument('dir', type=str, help='The path to a folder which contains 2 folders "img/" and "label/"')
+parser.add_argument('--flip', action='store_true',
+                    help='augment with flip image')
+parser.add_argument('--bright', action='store_true',
+                    help='augment with brightness on image')
+
 args = parser.parse_args()
 
 img_paths = glob.glob(args.dir + '/img/*')
 
 label_dir = args.dir + '/label/'
-flipped_img_dir = args.dir + '/img_f/'
-flipped_label_dir = args.dir + '/label_f/'
 
-if not os.path.isdir(flipped_img_dir):
-    os.mkdir(flipped_img_dir)
-if not os.path.isdir(flipped_label_dir):
-    os.mkdir(flipped_label_dir)
+if args.flip:
+    flipped_img_dir = args.dir + '/img_f/'
+    flipped_label_dir = args.dir + '/label_f/'
+
+    # # check and make new folder
+    if not os.path.isdir(flipped_img_dir):
+        os.mkdir(flipped_img_dir)
+    if not os.path.isdir(flipped_label_dir):
+        os.mkdir(flipped_label_dir)
+
+if args.bright:
+    bright_img_dir = args.dir + '/img_b/'
+    bright_label_dir = args.dir + '/label_b/'
+
+    if not os.path.isdir(bright_img_dir):
+        os.mkdir(bright_img_dir)
+    if not os.path.isdir(bright_img_dir):
+        os.mkdir(bright_img_dir)
+
+if not (args.flip or args.bright):
+    raise ValueError('No augment type given')
 
 for p in img_paths:
     img = Image.open(p)
@@ -29,15 +49,29 @@ for p in img_paths:
     with open(label_dir + 'label_' + '{:05d}'.format(img_id) + '.json', 'r') as f:
         label = json.load(f)
 
-    label_new = {'bbox': {}}
+    if args.flip:
+        label_new = {'bbox': {}}
+        for h in label['bbox']:
+            x0, y0, x1, y1 = label['bbox'][h]
+            hf = 'R' if h == 'L' else 'L'
+            label_new['bbox'][hf] = [width - x1, y0, width - x0, y1]
 
-    for h in label['bbox']:
-        x0, y0, x1, y1 = label['bbox'][h]
-        hf = 'R' if h == 'L' else 'L'
-        label_new['bbox'][hf] = [width - x1, y0, width - x0, y1]
+        img.transpose(Image.FLIP_LEFT_RIGHT).save(
+            flipped_img_dir + 'img_' + '{:05d}'.format(len(img_paths) + img_id) + '.png', 'PNG')
+        with open(flipped_label_dir + 'label_' + '{:05d}'.format(len(img_paths) + img_id) + '.json', 'w') as f:
+            json.dump(label_new, f)
 
-    img.transpose(Image.FLIP_LEFT_RIGHT).save(
-        flipped_img_dir + 'img_' + '{:05d}'.format(len(img_paths) + img_id) + '.png', 'PNG')
+    if args.bright:
+        brightness = ImageEnhance.Brightness(img)
+        bright_img = brightness.enhance(2.0)
+        darker_img = brightness.enhance(0.5)
 
-    with open(flipped_label_dir + 'label_' + '{:05d}'.format(len(img_paths) + img_id) + '.json', 'w') as f:
-        json.dump(label_new, f)
+        bright_img.save(
+            bright_img_dir + 'img' + '{:05d}'.format(len(img_paths) + img_id) + '.png', 'PNG')
+        with open(bright_label_dir + 'label_' + '{:05d}'.format(len(img_paths) + img_id) + '.json', 'w') as f:
+            json.dump(label, f)
+
+        darker_img.save(
+            bright_img_dir + 'img' + '{:05d}'.format(2 * len(img_paths) + img_id) + '.png', 'PNG')
+        with open(bright_label_dir + 'label_' + '{:05d}'.format(2 * len(img_paths) + img_id) + '.json', 'w') as f:
+            json.dump(label, f)
