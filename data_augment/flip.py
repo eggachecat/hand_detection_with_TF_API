cc976eac41aa2a59997ca64cb498d5ce0241b30e
rainyren <renokx@gmail.com>
2018-01-12 14:19:22 +0800

    small bug fix

--- data_augment/flip.py
+++ data_augment/flip.py
@@ -22,18 +22,18 @@ if not os.path.isdir(flipped_label_dir):
 for p in img_paths:
 	img = Image.open(p)
 	width, height = img.size
-	img_id = p.split('.')[0].split('_')[1]
-	
+	img_id = p.split('/')[-1].split('.')[0].split('_')[1]
+
 	with open(label_dir+'label_'+img_id+'.json','r') as f:
 		label = json.load(f)
-	
+
 	label_new = {'bbox':{}}
-	
+
 	for h in label['bbox']:
 		x0,y0,x1,y1 = label['bbox'][h]
 		hf = 'R' if h=='L' else 'L'
 		label_new['bbox'][hf] = [width-x1,y0,width-x0,y1]
-	
+
 	img.transpose(Image.FLIP_LEFT_RIGHT).save(flipped_img_dir+'img_'+img_id+'_f.png','PNG')
 	with open(flipped_label_dir+'label_'+img_id+'_f.json','w') as f:
 		json.dump(label_new,f)
